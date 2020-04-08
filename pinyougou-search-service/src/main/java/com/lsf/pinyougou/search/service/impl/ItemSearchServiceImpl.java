a17ddd1c7dde32f707fc600642e03133039dfb27
linshaofeng <linshaofeng981015@gmail.com>
2020-04-08 17:53:45 +0800

    1.完成商品搜索前台的搜索结果按关键字高亮显示

--- pinyougou-search-service/src/main/java/com/lsf/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/lsf/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -5,12 +5,13 @@ import com.lsf.pinyougou.pojo.TbItem;
 import com.lsf.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -38,6 +39,9 @@ public class ItemSearchServiceImpl implements ItemSearchService {
         // 构造返回体
         Map<String, Object> map = new HashMap<>();
 
+        /*
+            搜索结果没有做高亮显示
+
         // 构造搜索条件
         Query query = new SimpleQuery("*:*");
         // keywords 是用户在商品搜索前台搜索框中输入的内容
@@ -51,7 +55,68 @@ public class ItemSearchServiceImpl implements ItemSearchService {
                 = solrTemplate.queryForPage(query, TbItem.class);
 
         // 封装查询后的数据
-        map.put("rows", itemList.getContent());
+        map.put("rows", itemList.getContent());*/
+
+
+        // 搜索结果高亮显示
+
+        // 构造搜索对象 query
+        HighlightQuery query = new SimpleHighlightQuery();
+
+        // 往 query 中添加高亮配置
+        HighlightOptions highlightOptions = new HighlightOptions().addField("item_title");
+        highlightOptions.setSimplePrefix("<em style='color:red'>");
+        highlightOptions.setSimplePostfix("</em>");
+        query.setHighlightOptions(highlightOptions);
+
+        // 往 query 中添加查询条件
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+
+        // 获取查询的高亮列表
+        HighlightPage<TbItem> page =
+                solrTemplate.queryForHighlightPage(query, TbItem.class);
+
+        // 通过 page.getContent() 获取的查询结果集合是原生的，没有任何高亮修饰
+        //List<TbItem> content = page.getContent();
+
+        // 获取高亮列表的高亮入口集合
+        List<HighlightEntry<TbItem>> highlightBeginList
+                = page.getHighlighted();
+        // 循环高亮入口集合
+        for (HighlightEntry<TbItem> h : highlightBeginList) {
+            /*
+            // 获取所有的高亮域（对于每个实体，可能设置多个高亮域，我们这里只设置一个高亮域 item_title）
+            List<HighlightEntry.Highlight> hh = h.getHighlights();
+            // 遍历高亮域集合
+            for (HighlightEntry.Highlight hhh : hh) {
+                // 获取每个高亮域的高亮结果，对于每个高亮域，我们可能设置多个前缀和后缀，这里我们只设置一个 <em></em>
+                List<String> snipplets = hhh.getSnipplets();
+                System.out.println(snipplets.get(0));   // ...<em stype=>item_title</em>...
+            }*/
+
+            // 获取原实体类
+            TbItem item = h.getEntity();
+            // 获取高亮域集合
+            List<HighlightEntry.Highlight> hh = h.getHighlights();
+            if (hh != null && hh.size() > 0) {
+                // 高亮域集合存在，且有元素（我们这里只设置一个高亮域 item_title，所以集合的长度为 1）
+
+                // 获取高亮域的高亮结果集合
+                // 因为高亮域集合长度为 1，所以直接 get(0) 获取高亮结果集合
+                List<String> snipplets = hh.get(0).getSnipplets();
+
+                if (snipplets != null && snipplets.size() > 0) {
+                    // 高亮结果集合存在，且有元素（我们只一个前缀和后缀 <em></em>，所以高亮结果集合长度为 1）
+                    String reslut = snipplets.get(0);
+                    // 设置到原生实体类中
+                    item.setTitle(reslut);
+                    // 此处修改的实体类，直接会影响到 page.getContent() 中的实体类
+                }
+            }
+        }
+
+        map.put("rows", page.getContent());
 
         return map;
     }
--- pinyougou-search-web/src/main/webapp/js/base.js
+++ pinyougou-search-web/src/main/webapp/js/base.js
@@ -1,2 +1,13 @@
 // 基础模块 pinyougou
 var pyg = angular.module('pinyougou', []);
+
+// 添加过滤器
+// 过滤器名称 trustHtml
+// 过滤器依赖 $sce 服务
+// data 表示原始的，不受信任的 html 代码
+// trustAsHtml(data) 表示对 data 内容进行信任，并将信任的结果返回
+pyg.filter('trustHtml', ['$sce', function ($sce) {
+    return function (data) {
+        return $sce.trustAsHtml(data);
+    }
+}]);
